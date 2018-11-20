8c4f94fe64d4eb80ae5fd0d50d3d612569491c3d
SombraXxxX <43217593+SombraXxxX@users.noreply.github.com>
2018-11-19 23:36:46 -0500

    Update ATM.java

--- ATM.java
+++ ATM.java
@@ -40,9 +40,31 @@ public class ATM {
 		// ask user for pin
 		System.out.println("Pin Number: ");
 		int inpin = in.nextInt();
+		atm.menu():
+	}
+	public void menu(){
 		// validate that account number and pin match
 		if(inAccountNum == AccountNumber || inpin == Pin) {
-		
+			System.out.println("Type 1 for deposit, 2 for withdraw, 3 for view balance, 4 for exit");
+			int menunum = in.nextInt(); 
+		if ( menunum == 1) {
+			System.out.println("How much will you be depositing?");
+        		int deposit = in.nextInt();
+			int currentBalance = deposit + Balance;
+			System.out.println("Updated Balance: $" + currentBalance);
+			atm.menu();
+		}if(menunum == 2) {
+			System.out.println("How much will you be withdrawing?");
+        		int withdraw = in.nextInt();
+			int currentBalance = Balance - withdraw;
+			System.out.println("Updated Balance: $" + currentBalance);
+			atm.menu();
+		}if(menunum == 3) {
+			System.out.println("Current Balance: $" + Balance );
+		}if(menunum == 4 ){
+			atm.run();
+		}else{
+			atm.run();
 		}
 		// if valid account number and pin
 
